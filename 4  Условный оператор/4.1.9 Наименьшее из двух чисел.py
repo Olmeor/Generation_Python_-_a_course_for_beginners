014757b1e158a13cb14aad505b44a494f3a63d8b
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-01-08 19:20:45 +0700

    feat: add 4.1 solutions

---
+++ 4  Условный оператор/4.1.9 Наименьшее из двух чисел.py
@@ -0,0 +1,10 @@
+"""Наименьшее из двух чисел
+Напишите программу, которая определяет наименьшее из двух чисел."""
+
+# put your python code here
+a = int(input())
+b = int(input())
+if a < b:
+    print(a)
+else:
+    print(b)
