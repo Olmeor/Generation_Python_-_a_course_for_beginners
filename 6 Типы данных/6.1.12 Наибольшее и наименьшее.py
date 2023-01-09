609eac2f09bb775b9cdd98921580310ec361133d
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-01-09 22:02:18 +0700

    feat: add 6.1 solutions

---
+++ 6 Типы данных/6.1.12 Наибольшее и наименьшее.py
@@ -0,0 +1,7 @@
+# Наибольшее и наименьшее
+# Напишите программу, которая находит наименьшее и наибольшее из пяти чисел.
+
+# put your python code here
+a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
+print('Наименьшее число =', min(a, b, c, d, e))
+print('Наибольшее число =', max(a, b, c, d, e))
---
+++ 6 Типы данных/6.1.15 Абсолютная сумма.py
@@ -0,0 +1,6 @@
+# Абсолютная сумма
+# Даны пять чисел. Напишите программу, которая вычисляет сумму их модулей.
+
+# put your python code here
+a1, a2, a3, a4, a5 = float(input()), float(input()), float(input()), float(input()), float(input())
+print(abs(a1) + abs(a2) + abs(a3) + abs(a4) + abs(a5))
