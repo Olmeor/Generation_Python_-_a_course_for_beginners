099752b90e3e6967297af3be9c72821e1e58c309
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-01-10 19:24:30 +0700

    feat: add 7.1 solutions

---
+++ 7 Циклы for и while/7.1.2 Python is awesome.py
@@ -0,0 +1,6 @@
+# Python is awesome
+# Напишите программу, которая выводит слова «Python is awesome!» (без кавычек) 10 раз.
+
+# put your python code here
+for i in range(10):
+    print('Python is awesome!')
---
+++ 7 Циклы for и while/7.1.4 Последовательность символов.py
@@ -0,0 +1,12 @@
+# Последовательность символов
+# Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+
+# put your python code here
+for i in range(6):
+    print('A' * 3)
+for i in range(5):
+    print('B' * 4)
+print('E')
+for i in range(9):
+    print('T' * 5)
+print('G')
