fddfde04146b3c8632c5497ef755b302357f4274
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-01-16 23:27:54 +0700

    feat: add 9.5 solutions

---
+++ 9 Строковый тип данных/9.5.7 Дополните приведенный код.py
@@ -0,0 +1,4 @@
+# Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст:
+# «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
+
+print('In {}, someone paid {} {} for two pizzas.'.format(2010, '10k', 'Bitcoin'))
