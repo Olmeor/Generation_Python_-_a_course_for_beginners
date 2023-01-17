486e9364f48cf1ae888c5854dc091f0f5f7eae71
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-01-17 13:28:45 +0700

    feat: add 11.2 solutions

---
+++ 11 Списки/11.2.8 Дополните приведенный код.py
@@ -0,0 +1,5 @@
+# Дополните приведенный код так, чтобы он вывел элементы списка languages в обратном порядке.
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+print(languages[::-1])
